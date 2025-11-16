79ac5c6f9a43864123141641bbf5e5f2e0b7202e
tmori <kanetugu2015@gmail.com>
2025-11-16 10:26:23 +0900

    add ubuntu

--- launch.bash
+++ launch.bash
@@ -6,8 +6,17 @@ if [ "$#" -ne 1 ]; then
 fi
 
 CONFIG_FILE=$1
-export HAKO_DRONE_PROJECT_PATH=/Users/tmori/project/private/hakoniwa-drone-pro
-export HAKO_ENVSIM_PATH=/Users/tmori/project/oss/hakoniwa-envsim
-export PYTHON_PATH=/Users/tmori/.pyenv/versions/3.12.3/bin/python
-export HAKO_PANDA3D_DRONE_PATH=$(pwd)
+OS=$(uname)
+
+if [ "$OS" == "Darwin" ]; then
+    export HAKO_DRONE_PROJECT_PATH=/Users/tmori/project/private/hakoniwa-drone-pro
+    export HAKO_ENVSIM_PATH=/Users/tmori/project/oss/hakoniwa-envsim
+    export PYTHON_PATH=/Users/tmori/.pyenv/versions/3.12.3/bin/python
+    export HAKO_PANDA3D_DRONE_PATH=$(pwd)
+else
+    export HAKO_DRONE_PROJECT_PATH=~/hakoniwa/hakoniwa-drone-pro
+    export HAKO_ENVSIM_PATH=~/hakoniwa/hakoniwa-envsim
+    export PYTHON_PATH=~/venv-ardupilot/bin/python
+    export HAKO_PANDA3D_DRONE_PATH=$(pwd)
+fi
 python -m hakoniwa_pdu.apps.launcher.hako_launcher --mode immediate "$CONFIG_FILE"
