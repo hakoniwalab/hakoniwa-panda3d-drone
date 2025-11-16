303925a67207a233dcaf62bb8be68f37dffc5481
tmori <kanetugu2015@gmail.com>
2025-11-16 11:15:36 +0900

    updated the laundher for docker

--- launch.bash
+++ launch.bash
@@ -13,10 +13,12 @@ if [ "$OS" == "Darwin" ]; then
     export HAKO_ENVSIM_PATH=/Users/tmori/project/oss/hakoniwa-envsim
     export PYTHON_PATH=/Users/tmori/.pyenv/versions/3.12.3/bin/python
     export HAKO_PANDA3D_DRONE_PATH=$(pwd)
+    python -m hakoniwa_pdu.apps.launcher.hako_launcher --mode immediate "$CONFIG_FILE"
+
 else
-    export HAKO_DRONE_PROJECT_PATH=~/hakoniwa/hakoniwa-drone-pro
-    export HAKO_ENVSIM_PATH=~/hakoniwa/hakoniwa-envsim
-    export PYTHON_PATH=~/venv-ardupilot/bin/python
+    export HAKO_DRONE_PROJECT_PATH=${HOME}/workspace/hakoniwa-drone-pro
+    export HAKO_ENVSIM_PATH=${HOME}/workspace/hakoniwa-envsim
+    export PYTHON_PATH=python3
     export HAKO_PANDA3D_DRONE_PATH=$(pwd)
+    python3 -m hakoniwa_pdu.apps.launcher.hako_launcher --mode immediate "$CONFIG_FILE"
 fi
-python -m hakoniwa_pdu.apps.launcher.hako_launcher --mode immediate "$CONFIG_FILE"
